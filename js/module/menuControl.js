f6772c36aa85e13aa48d9c531fb568418fa1eb39
i-webtest <stasbass80@gmail.com>
2024-03-12 11:53:13 +0300

    Add: scripts for mobile menu

--- js/module/menuControl.js
+++ js/module/menuControl.js
@@ -2,18 +2,32 @@
 
 const menuBtn = document.querySelector('.hamburger');
 const menu = document.querySelector('.navigation');
-const menuLinks = menu.querySelectorAll('.navigation__link');
-const subMenu = menu.querySelector('.navigation__submenu');
-const subMenuLinks = subMenu.querySelectorAll('.navigation__submenu-link');
 const body = document.querySelector('body');
-const button = menu.querySelector('.navigation__button');
 const overlay = document.querySelector('.overlay');
 
 export const menuControl = () => {
   const toggleMenu = () => {
-    mobileBtn.classList.toggle('active');
+    menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
     body.classList.toggle('lock');
     overlay.classList.toggle('active');
   };
+
+  const handleMenu = ({ target }) => {
+    if (menu.classList.contains('active')) {
+      if (!target.closest('.navigation')) {
+        toggleMenu();
+      } else if (
+        (target.closest('.navigation') && target.closest('.navigation__link')) ||
+        target.closest('.navigation__submenu-link') ||
+        target.closest('.navigation__button')
+      ) {
+        toggleMenu();
+      }
+    } else if (target.closest('.hamburger')) {
+      toggleMenu();
+    }
+  };
+
+  document.addEventListener('click', handleMenu);
 };
